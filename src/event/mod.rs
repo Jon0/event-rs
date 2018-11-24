373457326ef7423ccf952c93ed4325c9105ea648
Jon0 <Jono4728@gmail.com>
2018-11-24 19:45:10 +1300

    using threads

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,28 +1,139 @@
 
+use std::collections::HashMap;
 use std::thread;
+use std::thread::JoinHandle;
+use std::sync::Arc;
 use std::sync::Mutex;
 use std::os::unix::io::RawFd;
+
+use nix::libc::off_t;
+use nix::fcntl::{open, OFlag};
 use nix::sys::epoll::*;
+use nix::sys::stat::Mode;
+use nix::sys::socket::{accept, bind, listen, recv, send, socket, shutdown};
+use nix::sys::socket::SockFlag;
+use nix::sys::socket::SockProtocol;
+use nix::sys::socket::{AddressFamily, MsgFlags, SockAddr, SockType, Shutdown, InetAddr, IpAddr};
+use nix::unistd::{lseek, read, Whence};
+
+
+pub struct EventHandler {
+
+}
+
+
+impl EventHandler {
+	pub fn new() -> EventHandler {
+		return EventHandler {};
+	}
+
+	pub fn exec(&self) {
+		println!("EventHandler");
+	}
+}
 
 
 
 pub struct EventLoop {
-	epoll: RawFd
+	epoll: RawFd,
+	next_id: u64,
+	event_map: HashMap<u64, EventHandler>,
+	thread_pool: Vec<JoinHandle<()>>
 }
 
 
 impl EventLoop {
-	fn new() {
-		let fd = epoll_create().unwrap();
+	pub fn new() -> EventLoop {
+		let epoll_fd = epoll_create().unwrap();
+
+		let event = EpollEvent::new(EpollFlags::EPOLLIN, 0);
+
 
 		let handle = thread::spawn(move || {
 			let mut event = [EpollEvent::empty()];
 
-			let count = epoll_wait(fd, &mut event, 0).unwrap();
+			let count = epoll_wait(epoll_fd, &mut event, -1).unwrap();
 
-			println!("{}", count);
+			println!("Recv: {}", count);
+			for c in 0..count {
+				let id = event[c].data();
+
+				println!("Data: {}", id);
+
+				// match self.event_map.get(id) {
+				// 	Some(handler) => handler.exec(),
+				// 	None => println!("No handler");
+				// }
+			}
 		});
 
 
+		return EventLoop {
+			epoll: epoll_fd,
+			next_id: 1,
+			event_map: HashMap::new(),
+			thread_pool: Vec::new()
+		}
+
 	}
+
+
+	pub fn create_threads(this: Arc<Mutex<Self>>) {
+
+		let a = this.clone();
+		let mut b = a.lock().unwrap();
+
+		let handle = thread::spawn(move || {
+			
+			let mut event = [EpollEvent::empty()];
+
+			let a = this.clone();
+			let emap = a.lock().unwrap();
+			let epoll_fd = emap.epoll;
+
+			let count = epoll_wait(epoll_fd, &mut event, -1).unwrap();
+
+			println!("Recv: {}", count);
+			for c in 0..count {
+				let id = event[c].data();
+
+				println!("Data: {}", id);
+
+				match emap.event_map.get(&id) {
+					Some(handler) => handler.exec(),
+					None => println!("No handler")
+				}
+			}
+		});
+
+		b.thread_pool.push(handle);
+	}
+
+
+	pub fn register(&mut self, fd: RawFd, handler: EventHandler) {
+		let id = self.next_id;
+		self.next_id += 1;
+		self.event_map.insert(id, handler);
+
+		// add to the epoll object
+		let mut event = EpollEvent::new(EpollFlags::EPOLLIN, id);
+		let c = epoll_ctl(self.epoll, EpollOp::EpollCtlAdd, fd, Some(&mut event));
+	}
+
+
+	pub fn listen(&mut self) {
+		let listen_fd = socket(AddressFamily::Inet, SockType::Stream, SockFlag::empty(), SockProtocol::Tcp).unwrap();
+		let b = bind(listen_fd, &SockAddr::new_inet(InetAddr::new(IpAddr::new_v4(0, 0, 0, 0), 8080))).unwrap();
+		let l = listen(listen_fd, 5);
+
+		self.register(listen_fd, EventHandler::new());
+	}
+
+
+	pub fn join(self) {
+		for t in self.thread_pool {
+			t.join();
+		}
+	}
+
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::env;
 use std::str;
 use inotify_sys::*;
 
+use event::*;
 use system::epoll::*;
 use system::file::*;
 use system::inotify::*;
@@ -29,36 +30,40 @@ fn connect() {
 
 
 fn main() {
-    let mut buf: [u8; 64] = [0; 64];
-    let args: Vec<String> = env::args().collect();
-    let mut epoll = EPoll::new().unwrap();
-    let mut timerfd = Timer::new().unwrap();
-    timerfd.set();
-    epoll.add_source(timerfd.as_fd());
 
-    for arg in &args[1..] {
-        println!("Listen to: {}", arg);
-
-        let file = FileDesc::open(arg).unwrap();
-        epoll.add_source(&file);
-
-        loop {
-            epoll.wait();
-            timerfd.read();
-            let result = file.read_at(&mut buf, 0).unwrap();
-            let content = str::from_utf8(&buf[0..result - 1]).unwrap();
-            println!("Read: {}", content);
-        }
-    }
+    let mut l = EventLoop::new();
+    l.listen();
+    l.join();
 
 
-    for arg in &args[1..] {
-        let mut inotify = INotify::new().unwrap();
-        let wd = inotify.add_watch(arg, IN_MODIFY).unwrap();
-        let event = inotify.read();
-        println!("Event: {:?}", wd);
-    }
+    // let mut buf: [u8; 64] = [0; 64];
+    // let args: Vec<String> = env::args().collect();
+    // let mut epoll = EPoll::new().unwrap();
+    // let mut timerfd = Timer::new().unwrap();
+    // timerfd.set();
+    // epoll.add_source(timerfd.as_fd());
+
+    // for arg in &args[1..] {
+    //     println!("Listen to: {}", arg);
+
+    //     let file = FileDesc::open(arg).unwrap();
+    //     epoll.add_source(&file);
+
+    //     loop {
+    //         epoll.wait();
+    //         timerfd.read();
+    //         let result = file.read_at(&mut buf, 0).unwrap();
+    //         let content = str::from_utf8(&buf[0..result - 1]).unwrap();
+    //         println!("Read: {}", content);
+    //     }
+    // }
 
 
+    // for arg in &args[1..] {
+    //     let mut inotify = INotify::new().unwrap();
+    //     let wd = inotify.add_watch(arg, IN_MODIFY).unwrap();
+    //     let event = inotify.read();
+    //     println!("Event: {:?}", wd);
+    // }
 
 }
